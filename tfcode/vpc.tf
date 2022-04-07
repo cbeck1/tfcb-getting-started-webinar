2d1cafebe3b4215a5ed120ae0615c4174afd1197
Chris Beck <cbeck@hashicorp.com>
2022-04-07 11:54:35 -0400

    remove tags

--- tfcode/vpc.tf
+++ tfcode/vpc.tf
@@ -14,10 +14,10 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Owner = "cbeck"
-    Name = "TFCB-Webinar"
-  }
+  #tags = {
+  #  Owner = "cbeck"
+  #  Name = "TFCB-Webinar"
+  #}
 }
 
 output "vpcid" {
